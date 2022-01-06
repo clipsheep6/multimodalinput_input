67e133c2f9c4c0f8cadf43103265d704f6991709
lisong <lisong92@huawei.com>
2022-01-06 10:01:11 +0800

    测试pr提交

--- frameworks/proxy/event_handle/src/client_msg_handler.cpp
+++ frameworks/proxy/event_handle/src/client_msg_handler.cpp
@@ -12,6 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 #include "client_msg_handler.h"
 #include <iostream>
 #include <inttypes.h>
