53b2b51e8feb80cc0d246bca80fc2826d3e26722
ywx1116641 <yangzhou42@huawei.com>
2022-01-29 16:21:47 +0800

    修改触摸屏监听检视代码
    Signed-off-by: ywx1116641 <yangzhou42@huawei.com>

--- service/input_monitor/src/input_handler_manager_global.cpp
+++ service/input_monitor/src/input_handler_manager_global.cpp
@@ -91,7 +91,7 @@ int32_t InputHandlerManagerGlobal::MonitorCollection::AddMonitor(const SessionMo
                  static_cast<int32_t>(monitors_.size()), INVALID_MONITOR_MON);
         return RET_ERR;
     }
-    auto iter = find(monitors_.begin(), monitors_.end(), mon);
+    auto iter = monitors.find(mon);
     if (iter != monitors_.end()) {
         MMI_LOGW("repeate register, errCode:%{public}d", INVALID_MONITOR_MON);
     }
