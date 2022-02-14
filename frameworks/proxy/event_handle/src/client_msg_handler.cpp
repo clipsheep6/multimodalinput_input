d119b0d62c02e1ef63caff3da4d95668abe06d6d
Justin_Hu <hugongcheng@huawei.com>
2022-02-14 03:32:20 +0000

    update frameworks/proxy/event_handle/src/client_msg_handler.cpp. Signed-off-by: Justin_Hu <hugongcheng@huawei.com>

--- frameworks/proxy/event_handle/src/client_msg_handler.cpp
+++ frameworks/proxy/event_handle/src/client_msg_handler.cpp
@@ -188,8 +188,8 @@ int32_t ClientMsgHandler::OnPointerEvent(const UDSClient& client, NetPacket& pkt
     }
 
     std::vector<int32_t> pointerIds { pointerEvent->GetPointersIdList() };
-    MMI_LOGD("pointer event dispatcher of client, eventType:%{public}d,actionTime:%{public}d, "
-             "action:%{public}d, actionStartTime:%{public}d, "
+    MMI_LOGD("pointer event dispatcher of client, eventType:%{public}d,actionTime:%{public}d,"
+             " action:%{public}d, actionStartTime:%{public}d, "
              "flag:%{public}d, pointerAction:%{public}s, sourceType:%{public}s, "
              "VerticalAxisValue:%{public}.2f, HorizontalAxisValue:%{public}.2f, "
              "PinchAxisValue:%{public}.2f, pointerCount:%{public}d, eventNumber:%{public}d",
