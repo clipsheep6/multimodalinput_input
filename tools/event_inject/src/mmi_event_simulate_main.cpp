3cf83d5c70148e09c31900c20c3ef5d2decb6ad7
lilong <lilong121@huawei.com>
2022-03-05 16:18:48 +0800

    修改代码检视意见：注入工具框架
    Signed-off-by: lilong <lilong121@huawei.com>

--- tools/event_inject/src/mmi_event_simulate_main.cpp
+++ tools/event_inject/src/mmi_event_simulate_main.cpp
@@ -25,7 +25,8 @@ int32_t main(int32_t argc, const char* argv[])
     do {
         OHOS::MMI::SetThreadName("main");
         if (argc < OHOS::MMI::ARGV_VALID) {
-            MMI_LOGI("Invaild Input Para, Plase Check the validity of the para! errCode:%d", OHOS::PARAM_INPUT_FAIL);
+            printf("Invaild Input Para, Plase Check the validity of the para! errCode:%{public}s",
+                OHOS::PARAM_INPUT_FAIL);
             break;
         }
         std::vector<std::string> argvs;
