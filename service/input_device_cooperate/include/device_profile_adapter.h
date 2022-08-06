c213f46c09bd09e8964f3303712d51eb1e2fd654
zhong.sui <zhong.sui@thundersoft.com>
2022-08-06 18:51:20 +0800

    添加键鼠穿越开关接口以及监听适配
    Change-Id: Ia32c6f8639a0f755b3dfb57fdf27044cc9658236
    Signed-off-by: zhong.sui <zhong.sui@thundersoft.com>

---
+++ service/input_device_cooperate/include/device_profile_adapter.h
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef DEVICE_PROFILE_ADAPTER_H
+#define DEVICE_PROFILE_ADAPTER_H
+
+#include <memory>
+#include <vector>
+#include <functional>
+
+#include "nocopyable.h"
+#include "singleton.h"
+#include "define_multimodal.h"
+#include "iprofile_event_callback.h"
+
+namespace OHOS {
+namespace MMI {
+class DeviceProfileAdapter : public DelayedSingleton<DeviceProfileAdapter> {
+public:
+    using ProfileEventCallback = std::function<void(const std::string &, bool)>;
+    DeviceProfileAdapter();
+    ~DeviceProfileAdapter();
+    DISALLOW_COPY_AND_MOVE(DeviceProfileAdapter);
+
+    /**
+     * Abandoned: it will cause this change can not notify other device
+     *
+     * @param state The new Switch state
+     * @return int32_t 0 : OK , Other: FAIL
+     */
+    int32_t UpdateCrossingSwitchState(bool state);
+
+    /**
+     * UpdateCrossingSwtichState
+     *
+     * @param state The new Switch state
+     * @param deviceIds Which device you want notify this change
+     * @return int32_t 0 : OK , Other: FAIL
+     */
+    int32_t UpdateCrossingSwitchState(bool state, std::vector<std::string> &deviceIds);
+
+    /**
+     * @brief Get the Crossing Switch State object
+     *
+     * @param deviceId The device you want to get
+     * @return true SwitchState
+     * @return false SwitchState
+     */
+    bool GetCrossingSwitchState(const std::string &deviceId);
+
+    /**
+     * Regist a listener to listen for one device switch change
+     *
+     * @param deviceId The device you want to listen
+     * @param callback The function for call you
+     * @return int32_t 0 : OK , Other: FAIL
+     */
+    int32_t RegisterCrossingStateListener(const std::string &deviceId, ProfileEventCallback callback);
+
+    /**
+     * Unregist the listener to listen for one device switch change
+     *
+     * @param deviceId The device you want to Unregist
+     * @param callback The which you set listen
+     * @return int32_t 0 : OK , Other: FAIL
+     */
+    int32_t UnregisterCrossingStateListener(const std::string &deviceId);
+
+private:
+    class ProfileEventCallbackImpl : public DeviceProfile::IProfileEventCallback {
+    public:
+        void OnSyncCompleted(const DeviceProfile::SyncResult &syncResults) override;
+        void OnProfileChanged(const DeviceProfile::ProfileChangeNotification &changeNotification) override;
+    };
+    void Init();
+    void UnInit();
+    int32_t RegisterProfileListener(const std::string &deviceId);
+    std::map<std::string, ProfileEventCallback> callbacks_;
+    std::shared_ptr<DeviceProfile::IProfileEventCallback> profileEventCallback_;
+};
+
+#define DProfileAdapter DeviceProfileAdapter::GetInstance()
+} // namespace MMI
+} // namespace OHOS
+
+#endif // DEVICE_PROFILE_ADAPTER_H
---
+++ service/input_device_cooperate/src/device_profile_adapter.cpp
@@ -0,0 +1,197 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "device_profile_adapter.h"
+
+#include "distributed_device_profile_client.h"
+#include "nlohmann/json.hpp"
+#include "service_characteristic_profile.h"
+#include "softbus_bus_center.h"
+#include "softbus_common.h"
+#include "sync_options.h"
+
+namespace OHOS {
+namespace MMI {
+using namespace OHOS::DeviceProfile;
+namespace {
+constexpr const char *SERVICE_ID = "InputDeviceCooperation";
+constexpr const char *SERVICE_TYPE = "InputDeviceCooperation";
+constexpr const char *CHARACTERISTICS_NAME = "currentState";
+} // namespace
+constexpr OHOS::HiviewDFX::HiLogLabel LABEL = {LOG_CORE, MMI_LOG_DOMAIN, "DeviceProfileAdapter"};
+
+DeviceProfileAdapter::DeviceProfileAdapter()
+{
+    Init();
+}
+
+DeviceProfileAdapter::~DeviceProfileAdapter()
+{
+    UnInit();
+}
+
+int32_t DeviceProfileAdapter::UpdateCrossingSwitchState(bool state, std::vector<std::string> &deviceIds)
+{
+    const std::string &serviceId = SERVICE_ID;
+    const std::string &serviceType = SERVICE_TYPE;
+    ServiceCharacteristicProfile profile;
+    profile.SetServiceId(serviceId);
+    profile.SetServiceType(serviceType);
+    nlohmann::json data;
+    data[CHARACTERISTICS_NAME] = state;
+    profile.SetCharacteristicProfileJson(data.dump());
+
+    int32_t putRet = DistributedDeviceProfileClient::GetInstance().PutDeviceProfile(profile);
+    SyncOptions syncOptions;
+    std::for_each(deviceIds.begin(), deviceIds.end(),
+                  [&, this](std::string &deviceId) { syncOptions.AddDevice(deviceId); });
+    int32_t syncRet =
+        DistributedDeviceProfileClient::GetInstance().SyncDeviceProfile(syncOptions, profileEventCallback_);
+    if (syncRet) {
+        MMI_HILOGW("Sync Device Profile failed code:%{public}d", syncRet);
+    }
+    return putRet;
+}
+
+int32_t DeviceProfileAdapter::UpdateCrossingSwitchState(bool state)
+{
+    const std::string &serviceId = SERVICE_ID;
+    const std::string &serviceType = SERVICE_TYPE;
+    ServiceCharacteristicProfile profile;
+    profile.SetServiceId(serviceId);
+    profile.SetServiceType(serviceType);
+    nlohmann::json data;
+    data[CHARACTERISTICS_NAME] = state;
+    profile.SetCharacteristicProfileJson(data.dump());
+    return DistributedDeviceProfileClient::GetInstance().PutDeviceProfile(profile);
+}
+
+bool DeviceProfileAdapter::GetCrossingSwitchState(const std::string &deviceId)
+{
+    bool result = false;
+    const std::string &serviceId = SERVICE_ID;
+    ServiceCharacteristicProfile profile;
+    DistributedDeviceProfileClient::GetInstance().GetDeviceProfile(deviceId, serviceId, profile);
+    std::string jsonData = profile.GetCharacteristicProfileJson();
+    nlohmann::json jsonObject = nlohmann::json::parse(jsonData, nullptr, false);
+    if (jsonObject.is_discarded()) {
+        MMI_HILOGE("JsonData is discarded");
+        return result;
+    }
+    result = jsonObject[CHARACTERISTICS_NAME].get<bool>();
+    return result;
+}
+
+int32_t DeviceProfileAdapter::RegisterCrossingStateListener(const std::string &deviceId, ProfileEventCallback callback)
+{
+    CHKPR(callback, RET_ERR);
+    if (deviceId.empty()) {
+        MMI_HILOGE("deviceId is NULL");
+        return RET_ERR;
+    }
+    auto callbackIter = callbacks_.find(deviceId);
+    if (callbackIter != callbacks_.end() && (&callbackIter->second == &callback)) {
+        MMI_HILOGI("callback has exist!!!");
+        return RET_OK;
+    }
+    callbacks_[deviceId] = callback;
+    MMI_HILOGI("Register crossing state listener success");
+    if (profileEventCallback_ == nullptr) {
+        profileEventCallback_ = std::make_shared<ProfileEventCallbackImpl>();
+    }
+    int32_t registerRet = RegisterProfileListener(deviceId);
+    if (registerRet != RET_OK) {
+        MMI_HILOGE("RegisterProfileListener failed error :%{public}d", registerRet);
+        return RET_ERR;
+    }
+    return RET_OK;
+}
+
+int32_t DeviceProfileAdapter::UnregisterCrossingStateListener(const std::string &deviceId)
+{
+    if (deviceId.empty()) {
+        MMI_HILOGE("deviceId is NULL");
+        return RET_ERR;
+    }
+    auto callbackIter = callbacks_.find(deviceId);
+    if (callbackIter == callbacks_.end()) {
+        MMI_HILOGE("This device has no callback");
+        return RET_OK;
+    }
+    callbacks_.erase(callbackIter);
+    return RET_OK;
+}
+
+int32_t DeviceProfileAdapter::RegisterProfileListener(const std::string &deviceId)
+{
+    std::list<SubscribeInfo> subscribeInfos;
+    std::list<std::string> serviceIdList;
+    serviceIdList.emplace_back(SERVICE_ID);
+    ExtraInfo extraInfo;
+    extraInfo["deviceId"] = deviceId;
+    extraInfo["serviceIds"] = serviceIdList;
+    SubscribeInfo changeEventInfo;
+    changeEventInfo.profileEvent = ProfileEvent::EVENT_PROFILE_CHANGED;
+    changeEventInfo.extraInfo = std::move(extraInfo);
+    subscribeInfos.emplace_back(changeEventInfo);
+
+    SubscribeInfo syncEventInfo;
+    syncEventInfo.profileEvent = ProfileEvent::EVENT_SYNC_COMPLETED;
+    subscribeInfos.emplace_back(syncEventInfo);
+
+    std::list<ProfileEvent> failedEvents;
+    int32_t ret = DistributedDeviceProfileClient::GetInstance().SubscribeProfileEvents(
+        subscribeInfos, profileEventCallback_, failedEvents);
+    MMI_HILOGI("Register Profile listener result: %{public}d", ret);
+    return ret;
+}
+
+void DeviceProfileAdapter::Init()
+{
+    profileEventCallback_ = std::make_shared<ProfileEventCallbackImpl>();
+}
+
+void DeviceProfileAdapter::UnInit()
+{
+    DistributedDeviceProfileClient::GetInstance().UnsubscribeProfileEvent(ProfileEvent::EVENT_PROFILE_CHANGED,
+                                                                          profileEventCallback_);
+    profileEventCallback_ = nullptr;
+    callbacks_.clear();
+}
+
+void DeviceProfileAdapter::ProfileEventCallbackImpl::OnProfileChanged(
+    const ProfileChangeNotification &changeNotification)
+{
+    MMI_HILOGD("OnProfile Changed!!!!");
+    std::string deviceId = changeNotification.GetDeviceId();
+    auto callbackIter = DProfileAdapter->callbacks_.find(deviceId);
+    if (callbackIter == DProfileAdapter->callbacks_.end()) {
+        MMI_HILOGD("The device has no callback");
+        return;
+    }
+    auto state = DProfileAdapter->GetCrossingSwitchState(deviceId);
+    if (callbackIter->second) {
+        callbackIter->second(deviceId, state);
+    } else {
+        DProfileAdapter->callbacks_.erase(callbackIter);
+    }
+}
+
+void DeviceProfileAdapter::ProfileEventCallbackImpl::OnSyncCompleted(const DeviceProfile::SyncResult &syncResults)
+{
+    MMI_HILOGI("OnSyncCompleted!!!!");
+}
+} // namespace MMI
+} // namespace OHOS
