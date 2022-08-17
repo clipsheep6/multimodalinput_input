bd6697fa0b77718b443ac279fd0bd8d36af5ef9f
fuchao0322 <fuchao33@huawei.com>
2022-08-17 20:09:55 +0800

    穿越管理-Napi-SC1
    Signed-off-by: fuchao0322 <fuchao33@huawei.com>

---
+++ frameworks/napi/input_device_cooperate/src/cooperate_start.cpp
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "cooperate_start.h"
+
+#include <cstddef>
+
+#include "constants.h"
+#include "define_multimodal.h"
+#include "input_manager.h"
+#include "mmi_log.h"
+#include "utils.h"
+#include "util_napi.h"
+
+namespace OHOS {
+namespace MMI {
+namespace CooperateStart {
+namespace {
+static constexpr OHOS::HiviewDFX::HiLogLabel LABEL = { LOG_CORE, MMI_LOG_DOMAIN, "cooperate_start" };
+} // namespace
+
+static sptr<Context> ParseParams(napi_env env, napi_callback_info info)
+{
+    MMI_HILOGI("Cooperate start: parse params");
+
+    size_t argc = 2;
+    napi_value argv[] = { nullptr, nullptr };
+    CHKRP(env, napi_get_cb_info(env, info, &argc, argv, nullptr, nullptr), GET_CB_INFO);
+
+    if (argc < 1 || argc > 2) {
+        THROWERR(env, "Cooperate start: parameter is not one or two or three");
+        return nullptr;
+    }
+
+    if (!TypeOf(env, argv[0], napi_string) || !TypeOf(env, argv[1], napi_number)) {
+        THROWERR(env, "Cooperate start: the first parameter is not string, the second parameter is not number");
+        return nullptr;
+    }
+
+    sptr<Context> context = new Context();
+
+    char sinkDeviceId[MAX_STRING_LEN] = {};
+    size_t length = 0;
+    CHKRP(env, napi_get_value_string_utf8(env, argv[0], sinkDeviceId, sizeof(sinkDeviceId), &length), GET_STRING);
+    context->sinkDeviceId_ = sinkDeviceId;
+
+    CHKRP(env, napi_get_value_int32(env, argv[1], &context->srcInputDeviceId_), GET_INT32);
+
+    return context;
+}
+
+static void Execute(sptr<Context> context)
+{
+    MMI_HILOGI("Cooperate start execute");
+
+    int32_t result = InputMgr->StartInputDeviceCooperate(context->sinkDeviceId_, context->srcInputDeviceId_);
+    if (result != RET_OK) {
+        MMI_HILOGE("Cooperate start execute error");
+    }
+
+}
+
+napi_value Start(napi_env env, napi_callback_info info)
+{
+    MMI_HILOGI("Cooperate start");
+
+    sptr<Context> context = ParseParams(env, info);
+    if (context == nullptr) {
+        MMI_HILOGE("Cooperate start: context is nullptr");
+        return nullptr;
+    }
+
+    Execute(context);
+    return nullptr;
+}
+} // namespace CooperateStart
+} // namespace MMI
+} // namespace OHOS
---
+++ frameworks/napi/input_device_cooperate/src/native_register_module.cpp
@@ -0,0 +1,126 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <napi/native_api.h>
+#include <napi/native_node_api.h>
+
+#include <cstdint>
+#include <cstddef>
+
+#include "constants.h"
+#include "cooperate_enable.h"
+#include "cooperate_get_state.h"
+#include "cooperate_messages.h"
+#include "cooperate_monitor.h"
+#include "cooperate_start.h"
+#include "cooperate_stop.h"
+#include "mmi_log.h"
+#include "util_napi.h"
+
+namespace OHOS {
+namespace MMI {
+namespace {
+constexpr OHOS::HiviewDFX::HiLogLabel LABEL = { LOG_CORE, MMI_LOG_DOMAIN, "native_register_module" };
+} // namespace
+
+static void DeclareDeviceCooperateInterface(napi_env env, napi_value exports)
+{
+    napi_property_descriptor functions[] = {
+        DECLARE_NAPI_STATIC_FUNCTION("enable", CooperateEnable::Enable),
+        DECLARE_NAPI_STATIC_FUNCTION("start", CooperateStart::Start),
+        DECLARE_NAPI_STATIC_FUNCTION("stop", CooperateStop::Stop),
+        DECLARE_NAPI_STATIC_FUNCTION("getState", CooperateGetState::GetState),
+        DECLARE_NAPI_STATIC_FUNCTION("on", CooperateMonitor::On),
+        DECLARE_NAPI_STATIC_FUNCTION("off", CooperateMonitor::Off),
+    };
+    CHKRV(env, napi_define_properties(env, exports,
+        sizeof(functions) / sizeof(*functions), functions), DEFINE_PROPERTIES);
+}
+
+static napi_value EnumClassConstructor(napi_env env, napi_callback_info info)
+{
+    size_t argc = 0;
+    napi_value args[1] = {nullptr};
+    napi_value result = nullptr;
+    void *data = nullptr;
+    CHKRP(env, napi_get_cb_info(env, info, &argc, args, &result, &data), GET_CB_INFO);
+    return result;
+}
+
+static napi_value CreateInt32(napi_env env, CooperationState msg)
+{
+    napi_value value = nullptr;
+    CHKRP(env, napi_create_int32(env, static_cast<int32_t>(msg), &value), CREATE_INT32);
+    return value;
+}
+
+static void DeclaerDeviceCooperateData(napi_env env, napi_value exports)
+{
+    napi_property_descriptor msg[] = {
+        DECLARE_NAPI_STATIC_PROPERTY("MSG_COOPERATE_OPEN_SUCCESS",
+            CreateInt32(env, CooperationState::MSG_COOPERATE_OPEN_SUCCESS)),
+        DECLARE_NAPI_STATIC_PROPERTY("MSG_COOPERATE_OPEN_FAIL",
+            CreateInt32(env, CooperationState::MSG_COOPERATE_OPEN_FAIL)),
+        DECLARE_NAPI_STATIC_PROPERTY("MSG_COOPERATE_INFO_START",
+            CreateInt32(env, CooperationState::MSG_COOPERATE_INFO_START)),
+        DECLARE_NAPI_STATIC_PROPERTY("MSG_COOPERATE_INFO_SUCCESS",
+            CreateInt32(env, CooperationState::MSG_COOPERATE_INFO_SUCCESS)),
+        DECLARE_NAPI_STATIC_PROPERTY("MSG_COOPERATE_INFO_FAIL",
+            CreateInt32(env, CooperationState::MSG_COOPERATE_INFO_FAIL)),
+        DECLARE_NAPI_STATIC_PROPERTY("MSG_COOPERATE_CLOSE",
+            CreateInt32(env, CooperationState::MSG_COOPERATE_CLOSE)),
+        DECLARE_NAPI_STATIC_PROPERTY("MSG_COOPERATE_CLOSE_SUCCESS",
+            CreateInt32(env, CooperationState::MSG_COOPERATE_CLOSE_SUCCESS)),
+        DECLARE_NAPI_STATIC_PROPERTY("MSG_COOPERATE_STOP",
+            CreateInt32(env, CooperationState::MSG_COOPERATE_STOP)),
+        DECLARE_NAPI_STATIC_PROPERTY("MSG_COOPERATE_STOP_SUCCESS",
+            CreateInt32(env, CooperationState::MSG_COOPERATE_STOP_SUCCESS)),
+        DECLARE_NAPI_STATIC_PROPERTY("MSG_COOPERATE_STOP_FAIL",
+            CreateInt32(env, CooperationState::MSG_COOPERATE_STOP_FAIL)),
+        DECLARE_NAPI_STATIC_PROPERTY("MSG_COOPERATE_STATE_ON",
+            CreateInt32(env, CooperationState::MSG_COOPERATE_STATE_ON)),
+        DECLARE_NAPI_STATIC_PROPERTY("MSG_COOPERATE_STATE_OFF",
+            CreateInt32(env, CooperationState::MSG_COOPERATE_STATE_OFF)),
+    };
+
+    napi_value eventMsg = nullptr;
+    CHKRV(env, napi_define_class(env, "eventMsg", NAPI_AUTO_LENGTH, EnumClassConstructor, nullptr,
+        sizeof(msg) / sizeof(*msg), msg, &eventMsg), DEFINE_CLASS);
+    CHKRV(env, napi_set_named_property(env, exports, "EventMsg", eventMsg), SET_NAMED_PROPERTY);
+}
+
+static napi_value RegisterDeviceCooperateInterface(napi_env env, napi_value exports)
+{
+    DeclareDeviceCooperateInterface(env, exports);
+    DeclaerDeviceCooperateData(env, exports);
+    return exports;
+}
+
+static napi_module mmiInputDeviceCooperateMoudle = {
+    .nm_version = 1,
+    .nm_flags = 0,
+    .nm_filename = nullptr,
+    .nm_register_func = RegisterDeviceCooperateInterface,
+    .nm_modname = "multimodalInput.inputDeviceCooperate",
+    .nm_priv = ((void *)0),
+    .reserved = {0},
+};
+
+extern "C" __attribute__((constructor)) void RegisterModule(void)
+{
+    napi_module_register(&mmiInputDeviceCooperateMoudle);
+}
+} // namespace MMI
+} // namespace OHOS
