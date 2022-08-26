e653bf1ced53331f5b8bdb396fd01c0ea013c614
gexiongfei <516780292@qq.com>
2022-08-26 14:37:03 +0800

    修改打印等级
    Signed-off-by: gexiongfei <516780292@qq.com>

--- service/module_loader/src/mmi_service.cpp
+++ service/module_loader/src/mmi_service.cpp
@@ -67,7 +67,7 @@ void CheckDefineOutput(const char* fmt, Ts... args)
     char buf[MAX_PACKET_BUF_SIZE] = {};
     int32_t ret = snprintf_s(buf, MAX_PACKET_BUF_SIZE, MAX_PACKET_BUF_SIZE - 1, fmt, args...);
     if (ret == -1) {
-        KMSG_LOGI("call snprintf_s fail.ret = %d", ret);
+        KMSG_LOGE("call snprintf_s fail.ret = %d", ret);
         return;
     }
     KMSG_LOGI("%s", buf);
