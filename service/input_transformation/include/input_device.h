95d97413b481159835797de561fed09073e44581
baixincheng <baixincheng@huawei.com>
2022-09-20 15:53:36 +0800

    add hdf normalize
    Signed-off-by: baixincheng <baixincheng@huawei.com>
    Change-Id: Ie5d6737aed599e0e94816c1832df85ad471e2624

---
+++ service/input_transformation/include/input_device.h
@@ -0,0 +1,126 @@
+/*
+ * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef INPUT_DEVICE_H
+#define INPUT_DEVICE_H
+
+#include <memory>
+#include <map>
+
+#include <linux/input-event-codes.h>
+#include <linux/input.h>
+
+#include "i_input_define.h"
+#include "i_input_device.h"
+// #include "IEventCollector.h"
+#include "i_seat.h"
+#include "i_touch_screen_seat.h"
+// #include "RelEventCollector.h"
+// #include "KeyEventCollector.h"
+#include "abs_event_collector.h"
+// #include "KernelKeyEvent.h"
+#include "abs_event.h"
+
+struct input_event;
+
+namespace OHOS {
+namespace MMI {
+
+class IKernelEventHandler;
+class InputDevice : public NonCopyable, public IInputDevice
+{
+    // public:
+        // static std::shared_ptr<InputDevice> Open(const std::string& deviceFile, const IInputContext* context);
+
+    public:
+        virtual ~InputDevice();
+        virtual int32_t GetId() const override;
+        virtual const std::string& GetName() const override;
+        virtual const std::string& GetSeatId() const override;
+        virtual const std::string& GetSeatName() const override;
+        virtual const std::string& GetDeviceFile() const override;
+        virtual std::shared_ptr<AxisInfo> GetAxisInfo(int32_t axis) const override;
+        virtual bool HasCapability(int32_t capability) const override;
+        virtual int32_t StartReceiveEvents(const std::shared_ptr<IKernelEventHandler>& eventHandler) override;
+        virtual int32_t StopReceiveEvents() override;
+
+    protected:
+        int32_t Init();
+        void Uninit();
+
+    private:
+        InputDevice(int32_t id, const std::string& deviceFile);
+
+    private:
+        void OnFdEvent(int fd, int event);
+        void ReadEvents();
+        int32_t CloseDevice();
+        void ProcessEventItem(struct input_event* eventItem);
+        int32_t UpdateCapablility();
+        int32_t UpdateInputProperty();
+        int32_t UpdateBitStat(int32_t evType, int32_t maxValue, unsigned long* resultValue, size_t len);
+        bool TestBit(int32_t bitIndex, const unsigned long* bitMap, size_t count) const;
+        bool HasInputProperty(int32_t property);
+        bool HasMouseCapability();
+        bool HasKeyboardCapability();
+        bool HasTouchscreenCapability();
+        bool HasTouchpadCapability();
+
+        bool HasEventType(int32_t evType) const;
+        bool HasEventCode(int32_t evType, int32_t evCode) const;
+
+        void ProcessSyncEvent(int32_t code, int32_t value);
+        // void ProcessKeyEvent(int32_t code, int32_t value);
+        // void ProcessRelEvent(int32_t code, int32_t value);
+        void ProcessAbsEvent(int32_t code, int32_t value);
+        void ProcessMscEvent(int32_t code, int32_t value);
+        // void ProcessSwEvent(int32_t code, int32_t value);
+        // void ProcessLedEvent(int32_t code, int32_t value);
+        // void ProcessSndEvent(int32_t code, int32_t value);
+        // void ProcessRepEvent(int32_t code, int32_t value);
+        // void ProcessFfEvent(int32_t code, int32_t value);
+        // void ProcessPwrEvent(int32_t code, int32_t value);
+        // void ProcessFfStatusEvent(int32_t code, int32_t value);
+
+        // void OnEventCollected(const std::shared_ptr<const RelEvent>& event);
+        // void OnEventCollected(const std::shared_ptr<const KernelKeyEvent>& event);
+        void OnEventCollected(const std::shared_ptr<const AbsEvent>& event);
+
+    private:
+        const int32_t id_;
+        const std::string deviceFile_;
+        // const IInputContext* const context_;
+        int32_t fd_;
+        std::string seatId_;
+        std::string seatName_;
+        std::string name_;
+
+        int32_t capabilities_ {IInputDevice::CAPABILITY_UNKNOWN};
+        unsigned long inputProperty[LongsOfBits(INPUT_PROP_MAX)];
+        unsigned long evBit[LongsOfBits(EV_MAX)];
+        unsigned long relBit[LongsOfBits(REL_MAX)];
+        unsigned long absBit[LongsOfBits(ABS_MAX)];
+
+        // RelEventCollector relEventCollector_;
+        // KeyEventCollector keyEventCollector_;
+        AbsEventCollector absEventCollector_;
+
+        mutable std::map<int32_t, std::shared_ptr<IInputDevice::AxisInfo>> axises_;
+
+        std::shared_ptr<IKernelEventHandler> eventHandler_;
+};
+} // namespace MMI
+} // namespace OHOS
+#endif // INPUT_DEVICE_H
---
+++ service/input_transformation/include/touch_screen_seat.h
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <memory>
+#include <map>
+
+#include "ITouchScreenSeat.h"
+#include "IInputDefine.h"
+#include "IInputDevice.h"
+#include "AbsEvent.h"
+#include "LogicalDisplayState.h"
+
+namespace Input {
+
+    class TouchScreenSeat : public NonCopyable, public ITouchScreenSeat {
+        public:
+            static std::unique_ptr<TouchScreenSeat> CreateInstance(const IInputContext* context, 
+                    const std::string& seatId, const std::string& seatName);
+
+        public:
+            virtual ~TouchScreenSeat() = default;
+
+            virtual const std::string& GetSeatId() const override;
+            virtual const std::string& GetSeatName() const override;
+            virtual bool IsEmpty() const override;
+
+            virtual int32_t BindInputDevice(const std::shared_ptr<IInputDevice>& inputDevice) override;
+            virtual int32_t UnbindInputDevice(const std::shared_ptr<IInputDevice>& inputDevice) override;
+            virtual std::shared_ptr<IInputDevice> GetInputDevice() const override;
+
+            virtual int32_t BindDisplay(const std::shared_ptr<PhysicalDisplayState>& display) override;
+            virtual int32_t UnbindDisplay(const std::shared_ptr<PhysicalDisplayState>& display) override;
+            virtual int32_t UpdateDisplay(const std::shared_ptr<PhysicalDisplayState>& display) override;
+            virtual std::shared_ptr<PhysicalDisplayState> GetDisplay() const override;
+
+            virtual void OnDisplayAdded(const std::shared_ptr<LogicalDisplayState>& display) override;
+            virtual void OnDisplayRemoved(const std::shared_ptr<LogicalDisplayState>& display) override;
+            virtual void OnDisplayChanged(const std::shared_ptr<LogicalDisplayState>& display) override;
+
+            virtual void OnInputEvent(const std::shared_ptr<const AbsEvent>& event) override;
+
+        protected:
+            TouchScreenSeat(const IInputContext* context, const std::string& seatId, const std::string& seatName);
+
+            std::shared_ptr<PhysicalDisplayState> GetTargetDisplay() const;
+
+            int32_t TransformToPhysicalDisplayCoordinate(int32_t tpX, int32_t tpY, int32_t& displayX, int32_t& displayY) const;
+
+            int32_t DispatchTo(const std::shared_ptr<const LogicalDisplayState>& targetDisplay, 
+                    int32_t pointerAction, int64_t actionTime, std::shared_ptr<PointerEvent::PointerItem>& pointer);
+
+            std::shared_ptr<PointerEvent::PointerItem> ConvertPointer(const std::shared_ptr<const AbsEvent>& absEvent, 
+                    int32_t& pointerAction, int64_t& actionTime, std::shared_ptr<const LogicalDisplayState>& targetDisplay);
+            int32_t ConvertAction(int32_t absEventAction) const;
+
+        private:
+            const IInputContext* const context_;
+            const std::string seatId_;
+            const std::string seatName_;
+
+            int32_t displayId_ {-1};
+            std::shared_ptr<IInputDevice> inputDevice_;
+            std::shared_ptr<PhysicalDisplayState> display_;
+            std::shared_ptr<IInputDevice::AxisInfo> xInfo_;
+            std::shared_ptr<IInputDevice::AxisInfo> yInfo_;
+            std::map<int64_t, std::shared_ptr<const LogicalDisplayState>> targetDisplays_;
+    };
+
+}
