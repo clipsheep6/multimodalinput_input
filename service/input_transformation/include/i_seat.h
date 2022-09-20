c5e4a766c199ef9b99d68cceae084e5cce750efb
baixincheng <baixincheng@huawei.com>
2022-09-20 09:29:16 +0800

    add hdf normalize
    Signed-off-by: baixincheng <baixincheng@huawei.com>
    Change-Id: I93e3d72a5c3b5207ff8e142db81c6e61a1acfac6

---
+++ service/input_transformation/include/i_seat.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef I_SEAT_H
+#define I_SEAT_H
+
+#include <memory>
+#include <list>
+
+namespace OHOS {
+namespace MMI {
+    class ISeat {
+        public:
+            static std::unique_ptr<ISeat> CreateInstance(const std::string& seatId);
+
+            virtual ~ISeat() = default;
+
+            virtual const std::string& GetSeatId() const = 0;
+
+            // virtual int32_t AddDisplay(const std::shared_ptr<LogicalDisplayState>& display) = 0;
+            // virtual int32_t RemoveDisplay(const std::shared_ptr<LogicalDisplayState>& display) = 0;
+            // virtual int32_t UpdateDisplay(const std::shared_ptr<LogicalDisplayState>& display) = 0;
+            // virtual std::list<std::shared_ptr<LogicalDisplayState>> GetDisplays() const = 0;
+
+            virtual int32_t AddInputDevice(const std::shared_ptr<IInputDevice>& inputDevice) = 0;
+            virtual int32_t RemoveInputDevice(const std::shared_ptr<IInputDevice>& inputDevice) = 0;
+            virtual std::list<std::shared_ptr<IInputDevice>> GetInputDevices() const = 0;
+
+            virtual bool IsEmpty() const = 0;
+
+            // virtual void OnInputEvent(const std::shared_ptr<const RelEvent>& event) = 0;
+            // virtual void OnInputEvent(const std::shared_ptr<const KernelKeyEvent>& event) = 0;
+            virtual void OnInputEvent(const std::shared_ptr<const AbsEvent>& event) = 0;
+    };
+
+} // namespace MMI
+} // namespace OHOS
+#endif // SEAT_H
---
+++ service/input_transformation/include/kernel_event_base.h
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef I_KERNEL_EVENT_BASE
+#define I_KERNEL_EVENT_BASE
+
+#include <cstdint>
+#include <ostream>
+#include <memory>
+
+#include "StreamUtils.h"
+
+struct input_event;
+namespace OHOS {
+namespace MMI {
+
+    class KernelEventBase {
+        public:
+            virtual ~KernelEventBase() = default;
+
+            int32_t GetDeviceId() const;
+            int32_t GetAction() const;
+            int64_t GetActionTime() const;
+            virtual std::ostream& operator<<(std::ostream& outStream) const;
+
+            void SetAction(int32_t action);
+            void SetActionTime(int64_t actionTime);
+
+        protected:
+            KernelEventBase(int32_t deviceId, int32_t action);
+            virtual const char* ActionToStr(int32_t action) const = 0;
+
+            std::ostream& PrintInternal(std::ostream& outStream) const;
+
+        private:
+            int32_t deviceId_;
+            int32_t action_;
+            int64_t actionTime_;
+    };
+
+    std::ostream& operator<<(std::ostream& outStream, const KernelEventBase& event);
+
+} // namespace MMI
+} // namespace OHOS
+#endif // I_KERNEL_EVENT_BASE
+
+
