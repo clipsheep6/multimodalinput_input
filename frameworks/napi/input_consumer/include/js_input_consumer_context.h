51b6516ef7ba94bca3277479498ecceff774a82d
mayunteng <mayunteng@huawei.com>
2023-01-03 02:17:13 +0000

    napi Refactoring - Key subscribe
    Signed-off-by: mayunteng <mayunteng@huawei.com>
    Change-Id: Ic989c2e5e57287b7fddc5bf3c85f23b52148c044

--- frameworks/napi/input_consumer/include/js_input_consumer_context.h
+++ frameworks/napi/input_consumer/include/js_input_consumer_context.h
@@ -12,6 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 #ifndef JS_INPUT_CONSUMER_CONTEXT_H
 #define JS_INPUT_CONSUMER_CONTEXT_H
 
