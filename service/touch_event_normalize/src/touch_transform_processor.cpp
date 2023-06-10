00ba056744a55fb6edcd8042439f44015db67fd5
houchengyu <303089883@qq.com>
2023-06-10 15:34:02 +0800

    增加指关节类型&支持指关节拉起ability    Signed-off-by: pengyuan2023 <lipengyuan4@huawei.com>

--- service/touch_event_normalize/src/touch_transform_processor.cpp
+++ service/touch_event_normalize/src/touch_transform_processor.cpp
@@ -21,6 +21,7 @@
 #include "input_windows_manager.h"
 #include "mmi_log.h"
 
+#include "fingersense_manager.h"
 namespace OHOS {
 namespace MMI {
 namespace {
@@ -66,6 +67,10 @@ bool TouchTransformProcessor::OnEventTouchDown(struct libinput_event *event)
     item.SetPressure(pressure);
     item.SetLongAxis(longAxis);
     item.SetShortAxis(shortAxis);
+#ifdef OHOS_BUILD_ENABLE_FINGERSENSE_WRAPPER
+    TouchType *rawTouch = nullptr;
+    FingersenseMgr->KnuckleClassifyTouch(rawTouch);
+#endif // OHOS_BUILD_ENABLE_FINGERSENSE_WRAPPER
     int32_t toolType = GetTouchToolType(touch, device);
     item.SetToolType(toolType);
     item.SetPointerId(seatSlot);
