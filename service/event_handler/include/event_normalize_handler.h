272dc04d08757ef5af47071d9b9bccde2ac837b3
andrey.dedukhin <dedyukhin.andrey@huawei-partners.com>
2023-11-03 07:30:20 +0300

    Some bugs has been fixed in resampling module and unit test.
    Signed-off-by: andrey.dedukhin <dedyukhin.andrey@huawei-partners.com>
    Change-Id: I081600e18b6aa5cd8991846a912cfb351f6bad56

--- service/event_handler/include/event_normalize_handler.h
+++ service/event_handler/include/event_normalize_handler.h
@@ -54,7 +54,7 @@ private:
 private:
     int32_t timerId_ { -1 };
     std::set<int32_t> buttonIds_ {};
-    void ResetTouchUpEvent(std::shared_ptr<PointerEvent> pointerEvent, struct libinput_event *event);
+    void ResetTouchUpEvent(std::shared_ptr<PointerEvent> pointerEvent);
 };
 } // namespace MMI
 } // namespace OHOS
