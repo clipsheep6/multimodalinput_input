5ef7546a7f56feba32713926540de64ebddfdfa3
double_tree_1 <zhoujiajun15@h-partners.com>
2024-04-10 09:26:41 +0800

    add ut
    Signed-off-by: double_tree_1 <zhoujiajun15@h-partners.com>
    Change-Id: If8a3d541779d3ee4cb46edeead4162e1da59ac6c

---
+++ service/timer_manager/test/timer_manager_test.cpp
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+#include <gtest/gtest.h>
+#include <fstream>
+#include "timer_manager.h"
+#include "mmi_log.h"
+#include "uds_server.h"
+
+namespace OHOS {
+namespace MMI {
+namespace {
+using namespace testing::ext;
+constexpr int32_t rationId { 0 };
+constexpr int32_t rightcode { 0 }; 
+} // namespace
+
+class TimerManagerTest : public testing::Test {
+public:
+    static void SetUpTestCase(void) {}
+    static void TearDownTestCase(void) {}
+};
+
+void someFunction()
+{
+    return;
+}
+
+/**
+ * @tc.name: TimerManagerTest_ManagerTimer_001
+ * @tc.desc: Test AddTimer
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(TimerManagerTest, TimerManagerTest_ManagerTimer_001, TestSize.Level1)
+{
+    int32_t repeatCount = 3;
+    int32_t intervalMs  = 1000;
+    int32_t timerld = TimerMgr->AddTimer(intervalMs, repeatCount, someFunction);
+    EXPECT_EQ(timerld, rationId);
+}
+
+/**
+ * @tc.name: TimerManagerTest_ManagerTimer_002
+ * @tc.desc: Test RemoveTimer
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(TimerManagerTest, TimerManagerTest_ManagerTimer_002, TestSize.Level1)
+{
+    int32_t repeatCount = 3;
+    int32_t intervalMs  = 1000;
+    int32_t timerld = TimerMgr->AddTimer(intervalMs, repeatCount, someFunction);
+    ASSERT_EQ(TimerMgr->RemoveTimer(timerld), rightcode);
+}
+
+/**
+ * @tc.name: TimerManagerTest_ManagerTimer_003
+ * @tc.desc: Test ResetTimer
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(TimerManagerTest, TimerManagerTest_ManagerTimer_003, TestSize.Level1)
+{
+    int32_t repeatCount = 3;
+    int32_t intervalMs = 1000;
+    int32_t timerld = TimerMgr->AddTimer(intervalMs, repeatCount, someFunction);
+    int32_t result = TimerMgr->ResetTimer(timerld);
+    EXPECT_EQ(result, 0);
+}
+
+/**
+ * @tc.name: TimerManagerTest_ManagerTimer_004
+ * @tc.desc: Test IsExist
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(TimerManagerTest, TimerManagerTest_ManagerTimer_004, TestSize.Level1)
+{
+    int32_t repeatCount = 3;
+    int32_t intervalMs = 1000;
+    int32_t timerld = TimerMgr->AddTimer(intervalMs, repeatCount, someFunction);
+    ASSERT_TRUE(TimerMgr->IsExist(timerld));
+}
+} // namespace MMI
+} // namespace OHOS
