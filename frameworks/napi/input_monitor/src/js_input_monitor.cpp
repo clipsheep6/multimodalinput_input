9976270a862224c64b5707141686a1ef45232d0e
Justin_Hu <hugongcheng@huawei.com>
2024-04-17 01:40:42 +0000

    code review
    Signed-off-by: Justin_Hu <hugongcheng@huawei.com>

--- frameworks/napi/input_monitor/src/js_input_monitor.cpp
+++ frameworks/napi/input_monitor/src/js_input_monitor.cpp
@@ -1209,7 +1209,7 @@ void JsInputMonitor::OnPointerEvent(std::shared_ptr<PointerEvent> pointerEvent)
     if (!evQueue_.empty()) {
         uv_work_t *work = new (std::nothrow) uv_work_t;
         CHKPV(work);
-        MonitorInfo *monitorInfo = new MonitorInfo();
+        MonitorInfo *monitorInfo = new (std::nothrow) MonitorInfo();
         CHKPV(monitorInfo);
         monitorInfo->monitorId = monitorId_;
         monitorInfo->fingers = fingers_;
