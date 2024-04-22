2c3e68570d4c752e4534fe73f19bc8e68c282d7a
mayunteng_1 <mayunteng@huawei.com>
2024-04-22 11:50:06 +0800

    update
    Signed-off-by: mayunteng_1 <mayunteng@huawei.com>

--- frameworks/napi/pointer/src/js_pointer_manager.cpp
+++ frameworks/napi/pointer/src/js_pointer_manager.cpp
@@ -964,12 +964,9 @@ napi_value JsPointerManager::GetTouchpadRotateSwitch(napi_env env, napi_value ha
 napi_value JsPointerManager::SetMoveEventFilters(napi_env env, bool flag)
 {
     CALL_DEBUG_ENTER;
-    InputManager::GetInstance()->SetMoveEventFilters(flag);
+    int32_t ret = InputManager::GetInstance()->SetMoveEventFilters(flag);
     napi_value result = nullptr;
-    if (napi_get_undefined(env, &result) != napi_ok) {
-        MMI_HILOGE("Get undefined result is failed");
-        return nullptr;
-    }
+    CHKRP(napi_create_int32(env, ret, &result), CREATE_INT32);
     return result;
 }
 } // namespace MMI
--- interfaces/native/innerkits/proxy/include/extra_data.h
+++ interfaces/native/innerkits/proxy/include/extra_data.h
@@ -18,7 +18,6 @@
 
 #include <vector>
 
-
 namespace OHOS {
 namespace MMI {
 struct ExtraData {
